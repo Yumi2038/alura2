203c6dfcea75a3473f6dd41b7b21cdca07d1ea9d
Yumi2038 <00001144288290sp@al.educacao.sp.gov.br>
2024-10-11 08:50:02 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'Qual música favorita ',
+    'No Surprises'
+)
+
+criaCartao(
+    'pessoal',
+    'qual minha comida favorita?',
+    'lasanha'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Livro favorito',
+    'Jogos Vorazes'
+)
+
+criaCartao(
+    'Curiosidade',
+    'lugar que recomenda visitar',
+    'jardim botanico'
+)
